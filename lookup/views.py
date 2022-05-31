0dad4c866885978624baedcd5bcb4eb89d89fa6d
gump334 <terrell.jackson@aknorthstar.com>
2022-05-31 16:59:26 -0500

    added the api to the views.py and api varible to home.html page

--- lookup/views.py
+++ lookup/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
 def home(request):
-  return render(request, 'home.html', {})
+    import json, requests
+    
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=36109&distance=150&API_KEY=E537E122-F9FC-4355-9BFE-D5BC0BC080D1")
+    
+    try:
+      api = json.loads(api_request.content)
+    except Exception as e:
+      api = "Error..."
+      
+    return render(request, 'home.html', {'api': api})
 # Create your views here.
 
 def about(request):
-  return render(request, 'about.html', {})
+    return render(request, 'about.html', {})
